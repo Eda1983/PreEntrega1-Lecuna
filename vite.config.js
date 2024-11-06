1569b09578cc7a148dc7c88b5904180bf03790ca
Edith Lecuna <edithlecuna@gmail.com>
2024-11-06 20:24:14 -0300

    actualizar config github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Eda1983/PreEntrega1-Lecuna',
+  base: '/eda1983/preentrega1-lecuna/',
   plugins: [react()],
 });
